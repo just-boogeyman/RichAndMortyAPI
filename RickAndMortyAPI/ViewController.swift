e443082e7747a126795eb375984e44b883b205f1
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-10-01 19:49:01 +0700

    Initial Commit

---
+++ RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Ярослав Кочкин on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
